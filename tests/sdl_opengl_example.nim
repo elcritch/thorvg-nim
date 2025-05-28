43a70d5dd7e9cb00c32ac80baedd474176f88a5b
Jaremy Creechley <creechley@gmail.com>
2025-05-28 05:14:52 -0600

    add check for image

--- tests/sdl_opengl_example.nim
+++ tests/sdl_opengl_example.nim
@@ -43,10 +43,14 @@ discard sdl2.init(INIT_EVERYTHING)
 var screenWidth: cint = 640
 var screenHeight: cint = 480
 
+let engine = initThorEngine(threads = 4)
+
 var window = createWindow("SDL/OpenGL Skeleton", 100, 100, screenWidth, screenHeight, SDL_WINDOW_OPENGL or SDL_WINDOW_RESIZABLE)
 var context = window.glCreateContext()
 
-let engine = initThorEngine(threads = 4)
+let canvas = newGlCanvas()
+canvas.setTarget(context, 0, uint32(screenWidth), uint32(screenHeight), TVG_COLORSPACE_ABGR8888S)
+
 
 # # Initialize OpenGL
 discard glSetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE.cint)
@@ -107,83 +111,11 @@ proc testBasicFunctionality(canvas: GlCanvas) =
   canvas.push(circle)
   canvas.push(gradShape)
 
-
-
 proc reshape(newWidth: cint, newHeight: cint) =
   glViewport(0, 0, newWidth, newHeight)   # Set the viewport to cover the new window
   glMatrixMode(GL_PROJECTION)             # To operate on the projection matrix
   glLoadIdentity()                        # Reset
   gluPerspective(45.0, newWidth / newHeight, 0.1, 100.0)  # Enable perspective projection with fovy, aspect, zNear and zFar
-
-proc render() =
-  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT) # Clear color and depth buffers
-  glMatrixMode(GL_MODELVIEW)                          # To operate on model-view matrix
-  glLoadIdentity()                 # Reset the model-view matrix
-  glTranslatef(1.5, 0.0, -7.0)     # Move right and into the screen
-
-  # Render a cube consisting of 6 quads
-  # Each quad consists of 2 triangles
-  # Each triangle consists of 3 vertices
-
-  glBegin(GL_TRIANGLES)        # Begin drawing of triangles
-
-  # Top face (y = 1.0f)
-  glColor3f(0.0, 1.0, 0.0)     # Green
-  glVertex3f( 1.0, 1.0, -1.0)
-  glVertex3f(-1.0, 1.0, -1.0)
-  glVertex3f(-1.0, 1.0,  1.0)
-  glVertex3f( 1.0, 1.0,  1.0)
-  glVertex3f( 1.0, 1.0, -1.0)
-  glVertex3f(-1.0, 1.0,  1.0)
-
-  # Bottom face (y = -1.0f)
-  glColor3f(1.0, 0.5, 0.0)     # Orange
-  glVertex3f( 1.0, -1.0,  1.0)
-  glVertex3f(-1.0, -1.0,  1.0)
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f( 1.0, -1.0, -1.0)
-  glVertex3f( 1.0, -1.0,  1.0)
-  glVertex3f(-1.0, -1.0, -1.0)
-
-  # Front face  (z = 1.0f)
-  glColor3f(1.0, 0.0, 0.0)     # Red
-  glVertex3f( 1.0,  1.0, 1.0)
-  glVertex3f(-1.0,  1.0, 1.0)
-  glVertex3f(-1.0, -1.0, 1.0)
-  glVertex3f( 1.0, -1.0, 1.0)
-  glVertex3f( 1.0,  1.0, 1.0)
-  glVertex3f(-1.0, -1.0, 1.0)
-
-  # Back face (z = -1.0f)
-  glColor3f(1.0, 1.0, 0.0)     # Yellow
-  glVertex3f( 1.0, -1.0, -1.0)
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f(-1.0,  1.0, -1.0)
-  glVertex3f( 1.0,  1.0, -1.0)
-  glVertex3f( 1.0, -1.0, -1.0)
-  glVertex3f(-1.0,  1.0, -1.0)
-
-  # Left face (x = -1.0f)
-  glColor3f(0.0, 0.0, 1.0)     # Blue
-  glVertex3f(-1.0,  1.0,  1.0)
-  glVertex3f(-1.0,  1.0, -1.0)
-  glVertex3f(-1.0, -1.0, -1.0)
-  glVertex3f(-1.0, -1.0,  1.0)
-  glVertex3f(-1.0,  1.0,  1.0)
-  glVertex3f(-1.0, -1.0, -1.0)
-
-  # Right face (x = 1.0f)
-  glColor3f(1.0, 0.0, 1.0)    # Magenta
-  glVertex3f(1.0,  1.0, -1.0)
-  glVertex3f(1.0,  1.0,  1.0)
-  glVertex3f(1.0, -1.0,  1.0)
-  glVertex3f(1.0, -1.0, -1.0)
-  glVertex3f(1.0,  1.0, -1.0)
-  glVertex3f(1.0, -1.0,  1.0)
-
-  glEnd()  # End of drawing
-
-  window.glSwapWindow() # Swap the front and back frame buffers (double buffering)
 
 # Frame rate limiter
 
@@ -202,13 +134,8 @@ var
   evt = sdl2.defaultEvent
   runGame = true
 
-reshape(screenWidth, screenHeight) # Set up initial viewport and projection
+# reshape(screenWidth, screenHeight) # Set up initial viewport and projection
 
-#                               ##  a specific framebuffer object (FBO) or the main surface.
-
-# let fbo = createFbo(screenWidth, screenHeight)
-
-let canvas = newGlCanvas()
 
 while runGame:
   while pollEvent(evt):
@@ -223,8 +150,8 @@ while runGame:
         # reshape(newWidth, newHeight)
 
   # render()
-  canvas.setTarget(context, 0, uint32(screenWidth), uint32(screenHeight), TVG_COLORSPACE_ABGR8888S)
   testBasicFunctionality(canvas)
+  # canvas.setTarget(context, 0, uint32(screenWidth), uint32(screenHeight), TVG_COLORSPACE_ABGR8888S)
   canvas.draw(true)
   canvas.sync()
   # blitToScreen(fbo, 0, 0, uint32(screenWidth), uint32(screenHeight))
