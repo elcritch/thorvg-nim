eec3034cc16f995d48b9eadecf0c5ba491994c22
Jaremy Creechley <creechley@gmail.com>
2025-06-05 01:52:54 -0600

    cleanup

--- config.nims
+++ config.nims
@@ -1,1 +1,5 @@
 include "build.nims"
+# begin Nimble config (version 2)
+when withDir(thisDir(), system.fileExists("nimble.paths")):
+  include "nimble.paths"
+# end Nimble config
--- tests/windex_opengl_example.nim
+++ tests/windex_opengl_example.nim
@@ -1,14 +1,10 @@
 
-import sdl2
 import opengl
 import opengl/glu
 import thorvg, thorvg/[canvases, paints, shapes, gradients]
 import windex
 
 import figuro_opengl
-
-{.passc: gorge("pkg-config --cflags sdl2").}
-{.passl: gorge("pkg-config --libs sdl2").}
 
 when defined(macosx):
   {.passC: "-Wno-incompatible-function-pointer-types".}
@@ -19,28 +15,8 @@ elif defined(windows):
 else:
   {.passC: "-I/usr/local/include".}
 
-discard sdl2.init(INIT_EVERYTHING)
-
 var screenWidth: cint = 640
 var screenHeight: cint = 480
-
-proc setupOpenGL() =
-  # # Initialize OpenGL
-  discard glSetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE.cint)
-  discard glSetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4.cint)
-  discard glSetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1.cint)
-
-  var window = createWindow("SDL/OpenGL Skeleton", 100, 100, screenWidth, screenHeight, SDL_WINDOW_OPENGL or SDL_WINDOW_RESIZABLE)
-  var context = window.glCreateContext()
-
-  loadExtensions()
-
-  glClearColor(0.0, 0.0, 0.0, 1.0)                  # Set background color to black and opaque
-  glClearDepth(1.0)                                 # Set background depth to farthest
-  glEnable(GL_DEPTH_TEST)                           # Enable depth testing for z-culling
-  glDepthFunc(GL_LEQUAL)                            # Set the type of depth-test
-  glShadeModel(GL_SMOOTH)                           # Enable smooth shading
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST) # Nice perspective corrections
 
 proc setupWindow*(
     window: Window,
